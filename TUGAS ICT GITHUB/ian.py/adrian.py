0d1ef3cc97a2022ec2b2c52150118db6d0229fff
AdrianDzariatMaulana <153588155+AdrianDzariatMaulana@users.noreply.github.com>
2023-12-12 14:51:23 +0700

    Add files via upload

---
+++ TUGAS ICT GITHUB/ian.py/adrian.py
@@ -0,0 +1,7 @@
+panjang = int(input("masukan panjangnya persegi panjang :"))
+lebar = int(input("masukan lebar persegi panjang :"))
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+print("\nluas persegi panjang adalah :",luas)
+print("keliling persegi panjang adalah",keliling)
